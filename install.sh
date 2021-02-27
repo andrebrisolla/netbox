a7f18991045ee4daf5bf55fd4d2dddb5b70caab4
Andre Brisolla <andremuzel@gmail.com>
2021-02-26 22:31:50 -0300

    Upload Netbox configuration

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ check_cmd() {
 
     rc=$1
 
-    if test $rc -eq 1
+    if test $rc -eq 0
     then
         echo "ok"
     else
@@ -16,7 +16,7 @@ check_cmd() {
 
 install_docker() {
 
-    curl -s  https://get.docker.com | bash #2> /dev/null > /dev/null
+    curl -s  https://get.docker.com | bash 2> /dev/null > /dev/null
     check_cmd $?
 
 }
